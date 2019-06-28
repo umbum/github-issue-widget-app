e2dae1f5d064e62a295f0b510bcfe97ea06bdeb2
umbum <umbum7601@gmail.com>
2019-06-28 13:55:41 +0900

    Display a toast message and Change constructor param
    - Display a toast message when login is complete or failed.
    - Change constructor param type from Context to Application

--- app/src/main/java/com/tistory/umbum/github_issue_widget_app/viewmodel/OAuthLoginViewModel.kt
+++ app/src/main/java/com/tistory/umbum/github_issue_widget_app/viewmodel/OAuthLoginViewModel.kt
@@ -1,7 +1,9 @@
 package com.tistory.umbum.github_issue_widget_app.viewmodel
 
-import android.content.Context
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
 import android.util.Log
+import android.widget.Toast
 import com.tistory.umbum.github_issue_widget_app.CLIENT_ID
 import com.tistory.umbum.github_issue_widget_app.CLIENT_SECRET
 import com.tistory.umbum.github_issue_widget_app.DBG_TAG
@@ -10,9 +12,8 @@ import com.tistory.umbum.github_issue_widget_app.repository.AccessTokenRepositor
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
-class OAuthLoginViewModel(private val _context: Context) {
-    private val context = _context.applicationContext
-    private val accessTokenRepository = AccessTokenRepository(context)
+class OAuthLoginViewModel(app: Application): AndroidViewModel(app) {
+    private val accessTokenRepository = AccessTokenRepository(getApplication())
 
     fun resolveAccessToken(code: String) {
         GithubClient.githubApi.requestAccessToken(CLIENT_ID, CLIENT_SECRET, code)
@@ -20,13 +21,15 @@ class OAuthLoginViewModel(private val _context: Context) {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
                     accessTokenResponse ->
-                    Log.d(DBG_TAG, "|resolveAccessToken| ${accessTokenResponse}")
+                    Log.d(DBG_TAG, "OAuthLoginViewModel.resolveAccessToken: ${accessTokenResponse}")
                     accessTokenRepository.accessToken = accessTokenResponse.access_token
-                    // 여기서 위젯 업데이트하고... 창 닫고..
-                    // 로그인 완료 되었다는 toast 메시지같은건 여기서 하는게 맞나? 아닌가?
+                    Toast.makeText(getApplication(), "Signed in!", Toast.LENGTH_LONG).show()
+
+                    // 위젯 업데이트!
                 }, {
-                    Log.e(DBG_TAG, "|resolveAccessToken| ${it.message}")
+                    Log.e(DBG_TAG, "OAuthLoginViewModel.resolveAccessToken: ${it.message}")
                     it.printStackTrace()
+                    Toast.makeText(getApplication(), "Login Error", Toast.LENGTH_LONG).show()
                 })
     }
 }
