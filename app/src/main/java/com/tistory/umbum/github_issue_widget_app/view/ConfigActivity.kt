54c461744589c1753a8c19ee6aec06265f0d4537
umbum <umbum7601@gmail.com>
2019-07-13 16:12:13 +0900

    [#5] Get rid of ConfigViewModel to remove unnecessary MVVM structure

--- app/src/main/java/com/tistory/umbum/github_issue_widget_app/view/ConfigActivity.kt
+++ app/src/main/java/com/tistory/umbum/github_issue_widget_app/view/ConfigActivity.kt
@@ -1,21 +1,23 @@
 package com.tistory.umbum.github_issue_widget_app.view
 
-import android.arch.lifecycle.ViewModelProviders
-import android.databinding.DataBindingUtil
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import com.tistory.umbum.github_issue_widget_app.R
-import com.tistory.umbum.github_issue_widget_app.databinding.ActivityConfigBinding
-import com.tistory.umbum.github_issue_widget_app.viewmodel.ConfigViewModel
 
 class ConfigActivity : AppCompatActivity() {
-    private val viewModel: ConfigViewModel by lazy {
-        ViewModelProviders.of(this).get(ConfigViewModel::class.java)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityConfigBinding = DataBindingUtil.setContentView(this, R.layout.activity_config);
-        binding.viewmodel = viewModel
+
+        setContentView(R.layout.activity_config)
+        setResult(Activity.RESULT_CANCELED)
+        val login_btn = findViewById<Button>(R.id.login_btn)
+        login_btn.setOnClickListener {
+            val intent = Intent(this, OAuthLoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
